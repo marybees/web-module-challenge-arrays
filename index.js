0ab434c8cb1292b45c441a0a2f8bb05868d1a010
Mary Baker <meb601@gmail.com>
2020-07-15 17:39:46 -0400

    Task 6 attempt

--- index.js
+++ index.js
@@ -115,8 +115,10 @@ Hint: You can use .splice() for this
 */
 
 function removeFlavorByName(arr, str) {
-    if (arr.includes(str)) {
-        return arr.splice(str, 2);
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === str) {
+            return arr.splice(str, 1)
+        }
     }
 }
 
@@ -131,11 +133,13 @@ Your function should accept:
 
 and should return a new array that is identical to the old array. You can name the new array however you'd like. */
 
-function copy(/*code here*/){
+let modifiedFlavors =  ["Rainbow Sherbert", "Banana Nut Fudge", "Black Walnut", "Burgundy Cherry", "Butterscotch Ribbon", "Cherry Macaron", "Chocolate", "Chocolate Almond", "Chocolate Chip", "Chocolate Fudge", "Chocolate Mint", "Chocolate Ribbon", "Coffee", "Coffee Candy", "Date Nut", "Eggnog", "French Vanilla", "Green Mint Stick", "Lemon Crisp", "Lemon Custard", "Lemon Sherbet", "Maple Nut", "Orange Sherbet", "Peach", "Peppermint Fudge Ribbon", "Peppermint Stick", "Pineapple Sherbet", "Raspberry Sherbet", "Rocky Road", "Strawberry"]
 
-    /*code here*/
-
+function copy(newArray, oldArray) {
+    newArray = [...oldArray];
 }
+
+console.log(copy(modifiedFlavors, originalFlavors));
 
 /* Task 7: July 7th is "World Chocolate Day" and Baskin Robins wants to create promotional materials highlighting all of their chocolate flavors. Write a function that checks every item in the array for a given string and returns a new array called filteredArray with just these values. Rather than hardcoding "chocolate" into your function, pass a string as a parameter, and invoke with the argument "chocolate". This way you could also filter for "Vanilla", "Sherbert", etc. when those holidays roll around.
 
